ea03feef38912d2121f64a2a660d6b6330c306ac
Elliot Dohm <elliot.dohm@shopify.com>
2019-06-11 14:58:14 -0400

    Add option to obfuscate object

--- cmd/x/cdn.go
+++ cmd/x/cdn.go
@@ -1,23 +1,41 @@
 package main
 
 import (
+	"encoding/base64"
 	"flag"
 	"fmt"
+	"math/rand"
 	"os"
 	"strings"
+	"time"
 )
 
 const basePath = "https://x.dohm.dev"
 
 var (
 	filePath, objectName string
+	hidden               bool
 )
 
 func init() {
 	flag.StringVar(&filePath, "path", "", "path to file")
 	flag.StringVar(&objectName, "name", "", "object name")
+	flag.BoolVar(&hidden, "hidden", false, "obfuscate url")
 
 	flag.Parse()
+}
+
+func generateRandString(n int) (string, error) {
+	rand.Seed(time.Now().UnixNano())
+
+	bytes := make([]byte, n)
+	_, err := rand.Read(bytes)
+
+	if err != nil {
+		return "", err
+	}
+
+	return base64.URLEncoding.EncodeToString(bytes), nil
 }
 
 func main() {
@@ -35,9 +53,20 @@ func main() {
 		os.Exit(1)
 	}
 
-	if objectName == "" {
+	if objectName == "" && !hidden {
 		var object = strings.Split(filePath, "/")
 		objectName = object[len(object)-1]
+	}
+
+	if hidden {
+		str, err := generateRandString(200)
+
+		if err != nil {
+			Log.Printf("error occurred during string generation %s", err)
+			os.Exit(1)
+		}
+
+		objectName = str
 	}
 
 	Log.Printf("%s: %s, %s: %s", "uploading file", filePath, "object", objectName)
